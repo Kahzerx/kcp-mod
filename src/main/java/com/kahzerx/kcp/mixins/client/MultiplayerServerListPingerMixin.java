409d8eb14f04c95c5f2f123f92625b408c982109
kahzerx <kahzerx@tutanota.com>
2023-01-01 16:51:55 +0100

    kcp ping fix

---
+++ src/main/java/com/kahzerx/kcp/mixins/client/MultiplayerServerListPingerMixin.java
@@ -0,0 +1,26 @@
+package com.kahzerx.kcp.mixins.client;
+
+import com.kahzerx.kcp.helpers.ServerInfoInterface;
+import com.kahzerx.kcp.protocol.KCPClientConnection;
+import com.kahzerx.kcp.protocol.Protocols;
+import net.minecraft.client.network.MultiplayerServerListPinger;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.network.ClientConnection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.net.InetSocketAddress;
+
+@Mixin(MultiplayerServerListPinger.class)
+public class MultiplayerServerListPingerMixin {
+    @Redirect(method = "add", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;connect(Ljava/net/InetSocketAddress;Z)Lnet/minecraft/network/ClientConnection;"))
+    private ClientConnection onServerPing(InetSocketAddress address, boolean useEpoll, final ServerInfo entry, final Runnable saver) {
+        Protocols protocol = ((ServerInfoInterface) entry).getProtocol();
+        if (protocol == Protocols.KCP) {
+            return KCPClientConnection.connect(address);
+        } else {
+            return ClientConnection.connect(address, useEpoll);
+        }
+    }
+}
