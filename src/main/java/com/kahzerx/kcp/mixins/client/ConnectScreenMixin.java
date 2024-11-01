aa8566b027d6dd5aa03626d16aad4b68d9c3c4e1
kahzerx <kahzerx@tutanota.com>
2024-11-01 21:41:17 +0100

    21 update

--- src/main/java/com/kahzerx/kcp/mixins/client/ConnectScreenMixin.java
+++ src/main/java/com/kahzerx/kcp/mixins/client/ConnectScreenMixin.java
@@ -6,6 +6,7 @@ import com.kahzerx.kcp.protocol.ServerInfoInterface;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
+import net.minecraft.client.network.CookieStorage;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,8 +21,8 @@ import java.net.ServerSocket;
 
 @Mixin(ConnectScreen.class)
 public class ConnectScreenMixin {
-    @Inject(method = "connect(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/multiplayer/ConnectScreen;connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;)V"))
-    private static void captureServerInfo(Screen screen, MinecraftClient client, ServerAddress address, ServerInfo info, boolean quickPlay, CallbackInfo ci) {
+    @Inject(method = "connect(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;ZLnet/minecraft/client/network/CookieStorage;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/multiplayer/ConnectScreen;connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;Lnet/minecraft/client/network/CookieStorage;)V"))
+    private static void captureServerInfo(Screen screen, MinecraftClient client, ServerAddress address, ServerInfo info, boolean quickPlay, CookieStorage cookieStorage, CallbackInfo ci) {
         Protocols actualProtocol = ((ServerInfoInterface) info).getProtocol();
         if (actualProtocol != Protocols.KCP) {
             return;
